c8b0b77ed0d8106acd0d5de8ad851217efadf61c
diegoHandy <62906658+diegoHandy@users.noreply.github.com>
2020-05-21 14:29:02 -0500

    Update percy.js

--- percy.js
+++ percy.js
@@ -9,27 +9,27 @@ PercyScript.run(async (page, percySnapshot) => {
   
 
   await page.goto('https://www.handy.la/');
-  await page.evaluate(scrollToBottom, { frequency: 99, timing: 70 });
+  await page.evaluate(scrollToBottom, { frequency: 99, timing: 30 });
   await percySnapshot('Home');
 
   
   await page.goto('https://www.handy.la/funcionalidades');
-  await page.evaluate(scrollToBottom, { frequency: 100, timing: 70 });
+  await page.evaluate(scrollToBottom, { frequency: 99, timing: 30 });
   await percySnapshot('funcionalidades');
 
   await page.goto('https://www.handy.la/preventa');
   await page.evaluate(scrollToBottom);
-  await page.evaluate(scrollToBottom, { frequency: 100, timing: 70 });
+  await page.evaluate(scrollToBottom, { frequency: 99, timing: 30 });
   await percySnapshot('preventa');
 
   await page.goto('https://www.handy.la/promotoria');
   await page.evaluate(scrollToBottom);
-  await page.evaluate(scrollToBottom, { frequency: 100, timing: 70 });
+  await page.evaluate(scrollToBottom, { frequency: 99, timing: 30 });
   await percySnapshot('promotoria');
 
   await page.goto('https://www.handy.la/venta-en-ruta');
   await page.evaluate(scrollToBottom);
-  await page.evaluate(scrollToBottom, { frequency: 100, timing: 70 });
+  await page.evaluate(scrollToBottom, { frequency: 99, timing: 30 });
   await percySnapshot('VentaRuta');
 
   let urls = ['https://www.handy.la/precios'];
